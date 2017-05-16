2b11bdb3ae649b1c90cd65f669ff2c804aab613a
zhjohow <zhjohow@163.com>
2017-05-17 00:05:18 +0800

    aa

---
+++ app/src/main/java/com/zhjh/selectphoto/MainActivity.java
@@ -0,0 +1,81 @@
+package com.zhjh.selectphoto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+
+import com.zhjh.selectphoto.adapter.GridAdapter;
+import com.zhjh.selectphoto.custom.CustomScrollGridView;
+import com.zhjh.selectphoto.utils.CommandPhotoUtil;
+import com.zhjh.selectphoto.utils.PhotoSystemOrShoot;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * 存放图片的容器
+     */
+    private CustomScrollGridView mGridView;
+
+    /**
+     * GridView适配器
+     */
+    private GridAdapter gridAdapter;
+
+    /**
+     * 管理图片操作
+     */
+    private CommandPhotoUtil commandPhoto;
+
+    /**
+     * 选择图片来源
+     */
+    private PhotoSystemOrShoot selectPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initUI();
+        addPlus();
+
+    }
+
+    /**
+     * 初始化UI
+     */
+    private void initUI() {
+        mGridView = (CustomScrollGridView) findViewById(R.id.gv_all_photo);
+    }
+
+    /**
+     * 实例化组件
+     */
+    private void addPlus() {
+        gridAdapter = new GridAdapter(MainActivity.this, 12);
+        mGridView.setAdapter(gridAdapter);
+
+        // 选择图片获取途径
+        selectPhoto = new PhotoSystemOrShoot(MainActivity.this) {
+            @Override
+            public void onStartActivityForResult(Intent intent, int requestCode) {
+                startActivityForResult(intent, requestCode);
+            }
+        };
+        commandPhoto = new CommandPhotoUtil(MainActivity.this, mGridView, gridAdapter, selectPhoto);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 获取照片返回
+        if (selectPhoto != null) {
+            String photoPath = selectPhoto.getPhotoResultPath(requestCode, resultCode, data);
+            if (!TextUtils.isEmpty(photoPath)) {
+                commandPhoto.showGridPhoto(photoPath);
+            }
+        }
+    }
+}
